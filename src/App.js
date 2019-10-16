7cda05cb889dd65ca2a76397bb56215178313314
djmac19 <49916861+djmac19@users.noreply.github.com>
2019-10-16 10:40:45 +0100

    set up socket (#3)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import * as api from "./api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {};
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log("in handleSubmit");
+    api.sendMessage(event.target.value);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="handle">Handle</label>
+        <input id="handle" type="text" />
+        <label htmlFor="message">Message</label>
+        <input id="message" type="text" />
+        <button type="submit">Send</button>
+      </form>
+    );
+  }
 }
 
 export default App;
